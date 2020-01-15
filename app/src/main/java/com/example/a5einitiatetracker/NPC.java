b5b5e15894f35f7671a9b17cc472ef23f93ad750
Logan.Royer <loganroyer@hotmail.com>
2020-01-15 14:05:22 -0800

    Updated NPC Intiative Rolling method for advantage and disadvantage functionality

--- app/src/main/java/com/example/a5einitiatetracker/NPC.java
+++ app/src/main/java/com/example/a5einitiatetracker/NPC.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 class NPC extends Combatant {
     protected int health;
+    protected enum advantage {NONE, ADVANTAGE, DISADVANTAGE}
     Random roller = new Random();
 
     public NPC(){
@@ -26,8 +27,24 @@ class NPC extends Combatant {
         this.health = health;
     }
 
-    private int rollInitiative(){
-        return roller.nextInt(21) + initiativeModifier;
+    //Method to roll intiative. Note for input 0 = no extra roll, 1 = advantage, -1 = disadvantage
+    private int rollInitiative(int adv){
+        int temp = roller.nextInt(21);
+        if(adv == 1){
+            int temp2 = roller.nextInt(21);
+            if(temp > temp2)
+                return temp  + initiativeModifier;
+            else
+                return temp2  + initiativeModifier;
+        }
+        else if (adv == -1){
+                int temp2 = roller.nextInt(21);
+                if(temp < temp2)
+                    return temp  + initiativeModifier;
+                else
+                    return temp2  + initiativeModifier;
+        }
+        else
+            return temp + initiativeModifier;
     }
-
 }
