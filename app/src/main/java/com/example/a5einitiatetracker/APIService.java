0409556e28b45737c41e2bd82d8bb290fffb80e3
Logan.Royer <loganroyer@hotmail.com>
2020-01-21 14:24:51 -0800

    Initial creation of branch Created APIService interface to help with API calls Edited manifest file to include internet permissions Edited the build.gradle file to remove unneeded implementations

---
+++ app/src/main/java/com/example/a5einitiatetracker/APIService.java
@@ -0,0 +1,15 @@
+package com.example.a5einitiatetracker;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface APIService {
+    @GET("monsters")
+    Call<List<Monster>> listMonsters();
+    @GET("monsters/{monster}")
+    Call<Monster> getMonsterStats(@Path("monster") String monster);
+
+}
