0dea009801e3289e8bb3fdde1373da1339ddb1fe
Logan <loganroyer@hotmail.com>
2020-03-09 21:50:44 -0700

    Pulled changed from master and added comments to new code

--- app/src/main/java/com/example/a5einitiatetracker/activities/CombatantsActivity.java
+++ app/src/main/java/com/example/a5einitiatetracker/activities/CombatantsActivity.java
@@ -94,8 +94,10 @@ public class CombatantsActivity extends AppCompatActivity {
     // loads all the monster info and adds them to the combatant list, then sorts the list by
     // initiative
     public void getMonsterData(View v) {
+        //If the combatants list is already created then clear it before adding new elements to it
         if(!combatantsList.isEmpty())
             combatantsList.clear();
+        //If the user tries to start an empty combat, prevent it and display a message
         if(parentLinearLayout.getChildCount() == 0){
             Toast.makeText(this.getApplicationContext(), "Please add at least one combatant to the combat to start it!", Toast.LENGTH_SHORT).show();
             return;
