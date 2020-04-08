00e7d7240def97e3ef8e9e50e75006d4bf88632c
Logan <loganroyer@hotmail.com>
2020-04-07 19:52:04 -0700

    Renamed package to com.dnd5e.combattracker for release

---
+++ app/src/main/java/com/dnd5e/combattracker/api/MonsterIndex.java
@@ -0,0 +1,30 @@
+package com.dnd5e.combattracker.api;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MonsterIndex {
+
+    //region VARIABLES
+    @SerializedName("results")
+    private List<MonsterName> results;
+
+    //endregion
+
+    //region PUBLIC FUNCTIONS/METHODS
+
+    public MonsterIndex(List<MonsterName> results) {
+        this.results = results;
+    }
+
+    public List<MonsterName> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MonsterName> results) {
+        this.results = results;
+    }
+
+    //endregion
+}
